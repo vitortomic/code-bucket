d317be4fdbb4029ed3bcb64de2720d0c3930cef0
vitortomic <vitortomic@gmail.com>
2017-02-23 15:31:15 +0100

    Delete poslovipolicenci.sql

--- sql/poslovipolicenci.sql
+++
@@ -1,3 +0,0 @@
-select count(id) as 'jobCount', licenceType_Id 
-from JobRequest where jobStatus = 'ACCEPTED' and dateInitiated>DATE_ADD(NOW(),INTERVAL -1 MONTH) 
-group by licenceType_id order by jobCount desc
