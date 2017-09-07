bfc1cfcc62508840d9f10ace6cebd5dd2553391b
Vitor Tomic <vitortomic@gmail.com>
2017-09-07 18:03:14 +0200

    added stomp service with angular defered example

---
+++ javascripts/angular/stompServiceAngularPromise.js
@@ -0,0 +1,105 @@
+app.factory('stompService', ['$q', '$timeout', '$location', '$localStorage', function($q, $timeout, $location, $localStorage){
+	var listener = $q.defer();
+	var connected = $q.defer();
+	var socket = {
+			client: null,
+			stomp: null
+	}
+	var service = {
+			connect : function(hospitalId, groupId){
+				initialize(hospitalId, groupId);
+			},
+			receive : function(){
+				return listener.promise;
+			},
+			send : function(message){
+				connected.promise.then(function(){
+					socket.stomp.send("/api/v1/chat/messages", {}, JSON.stringify(message));
+				});				
+			},
+			isConnected : function(){
+				if(!socket || !socket.stomp) return false;
+				return socket.stomp.connected;
+			}
+	};
+	
+	service.SOCKET_URL = $location.absUrl().replace($location.path(),"/chat").replace("/home#!","");
+	service.RECONNECT_TIMEOUT = 30000;
+	
+	var subscribeCallback = function(hospitalId, groupId){
+		connected.resolve();
+		// check user type u localstorage.userdetails and then filter 
+		
+		var currentUser = JSON.parse(localStorage.getItem("careUpdate.userDetails"));
+		
+		// in terms of subscription primary contact and receiver are the same user
+		var patientPrimary = currentUser.isPatient || currentUser.isPrimaryContact;
+		
+		var sender = currentUser.role === 'Sender';
+		
+		// subscribe user only on its topics:
+		
+		if(patientPrimary || sender){
+			socket.stomp.subscribe("/topic/careupdate." + hospitalId
+					+ '.' + groupId + ".updates.urgent", function(message){
+				listener.notify(getMessage(message));
+			});
+			if(patientPrimary){
+				socket.stomp.subscribe("/topic/careupdate." + hospitalId
+						+ '.' + groupId + ".updates.urgent.patient.primary", function(message){
+					listener.notify(getMessage(message));
+				});
+			}
+		}
+		
+		if(!sender){
+			socket.stomp.subscribe("/topic/careupdate." + hospitalId
+					+ '.' + groupId + ".messages", function(message){
+				listener.notify(getMessage(message));
+			});
+			socket.stomp.subscribe("/topic/careupdate." + hospitalId
+					+ '.' + groupId + ".updates.patient.contact", function(message){
+				listener.notify(getMessage(message));
+			});
+		}
+
+		socket.stomp.subscribe("/topic/careupdate." + hospitalId
+			    + '.' + groupId + ".updates", function(message){
+			listener.notify(getMessage(message));
+		});
+	};
+	
+	var getMessage = function(data) {
+	      var message = JSON.parse(data.body), out = {};
+	      out.message = message;
+	      return out;
+	};
+	
+	var reconnect = function(){
+		$timeout(function(){
+			initialize();
+		}, this.RECONNECT_TIMEOUT);
+	}
+
+	var initialize = function(hospitalId, groupId){
+		socket.client = new SockJS(service.SOCKET_URL);
+		socket.stomp = Stomp.over(socket.client);
+		socket.stomp.connect({}, subscribeCallback.bind(null, hospitalId, groupId));
+		socket.stomp.onclose = reconnect;
+	};
+	
+	return service;
+}]);
+
+/* //usage
+  var connectToStompService = function(){
+		if(!stompService.isConnected()){
+			var institutionId = $localStorage.currentUser.institution.id
+			stompService.connect(institutionId, $scope.patientId);
+		}
+		stompService.receive().then(null, null, function(response){
+			var message = formatUpdateMessage(response.message);
+			$scope.messages.push(message);
+		});
+	};
+*/
