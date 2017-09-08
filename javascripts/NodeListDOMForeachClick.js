e91c8a432048da428bf267e3341b18a6e7ec5460
Vitor Tomic <vitortomic@gmail.com>
2017-09-08 16:55:19 +0200

    Update NodeListDOMForeachClick.js

--- javascripts/NodeListDOMForeachClick.js
+++ javascripts/NodeListDOMForeachClick.js
@@ -1,1 +1,8 @@
 [].slice.call(document.querySelectorAll('.unfollow-text')).forEach(function(button) {button.click();});
+
+//ES6 version
+let nodeList = document.querySelectorAll('.unfollow-text')
+let arr = Array.from(nodeList);
+arr.forEach((element)=>element.click());
+
+Array.from(document.querySelectorAll('.unfollow-text')).forEach((element)=>element.click());
