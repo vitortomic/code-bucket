ea87618c896edf0a7d3d5d03500e07a5c43f046e
Vitor Tomic <vitortomic@gmail.com>
2017-09-20 16:22:49 +0200

    Update storageEvents.js

--- javascripts/storageEvents.js
+++ javascripts/storageEvents.js
@@ -1,3 +1,4 @@
+/* basic example
 window.addEventListener('storage', storage_handler, false);
 function storage_handler(evt)
 {
@@ -7,3 +8,20 @@ function storage_handler(evt)
     alert('The URL of the page that made the change was '+evt.url);
     alert('The window where the change was made was '+evt.source);
 }
+*/
+
+/**
+   Logout all other tabs on login
+**/
+
+//on application load
+window.addEventListener('storage', storage_handler, false);
+function storage_handler(event)
+{	
+	if(event.key == "logout_event"){
+		authService.logout();
+	}
+}
+
+//on login, using new date so the value is always changed and logout is performed
+localStorage.setItem("logout_event", new Date());
