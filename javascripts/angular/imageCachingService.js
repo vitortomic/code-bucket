09b37fe4790b195f6b4482dcc5128c83710c7a3c
Vitor Tomic <vitortomic@gmail.com>
2017-09-29 17:25:50 +0200

    Update imageCachingService.js

--- javascripts/angular/imageCachingService.js
+++ javascripts/angular/imageCachingService.js
@@ -1,9 +1,9 @@
 app.factory('imageCachingService', ['fileService','$q', function(fileService, $q){
 	
 	var cachedImages = new Map();
+	var promiseMap = new Map();
 	var promise = $q.defer();
 	
-	//userImageUrl contains file type extension while the GET route only requires the ID string
 	var getImageUrlWithoutExtenstion = function(userImageUrl){
 		var dotIndex = userImageUrl.indexOf(".");
 		return imageUrlWithoutExtension = userImageUrl.substring(0, dotIndex);
@@ -14,24 +14,27 @@ app.factory('imageCachingService', ['fileService','$q', function(fileService, $q
 	}
 	
 	var cacheImage = function(imgUrl){
-		return fileService.getFilePromiseBase64(imgUrl).then(function(response){
+		//if promise for the provided url was already made return it
+		var savedPromise = promiseMap.get(imgUrl);
+		if(savedPromise) return savedPromise;
+		//othervise get the img
+		var cachePromise = fileService.getFilePromiseBase64(imgUrl).then(function(response){
 			cachedImages.set(imgUrl, response);
 			return response;
 		});
+		promiseMap.set(imgUrl, cachePromise);
+		return cachePromise;
 	}
 	
 	return {
 		getImageUri : function(imgUrl){
-			//reject promise if no img url is provided
 			if(!imgUrl){
 				return $q.reject("No image url provided");
 			}
 			var imageUri = getImageById(getImageUrlWithoutExtenstion(imgUrl));
-			//if image is cached in the map wrap it into a promise and return
 			if(imageUri){
 				return $q.resolve(imageUri);
 			}
-			//if the image is not cached, return promise for fetching and caching the image
 			return cacheImage(getImageUrlWithoutExtenstion(imgUrl));
 		}
 	};
