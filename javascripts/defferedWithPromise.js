0efad103e1aa51f41c22473b0338d48caa8a6d46
Vitor Tomic <vitortomic@gmail.com>
2017-10-12 18:36:15 +0200

    Update defferedWithPromise.js

--- javascripts/defferedWithPromise.js
+++ javascripts/defferedWithPromise.js
@@ -23,3 +23,23 @@ var deffered = new Promise.deffered();
 deffered.promise.then((result)=> console.log(result));
 
 setTimeout(()=>{deffered.resolve("hello world")},10000);
+
+//refactored
+//deffered version
+Promise.deffered = function(){
+    var result = {};
+    result.promise = new Promise((resolve,reject)=>{
+        result.resolve = resolve;
+        result.reject = reject;
+    });
+    return result;
+};
+
+function getPromise(){
+    var deffered = new Promise.deffered();
+    setTimeout(()=>deffered.resolve("hello world"),1000);
+    return deffered.promise;
+}
+
+var promis = getPromise();
+promis.then((result)=>console.log(result));
