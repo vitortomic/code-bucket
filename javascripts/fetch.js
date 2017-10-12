641ca81ae7d5d201bafe9ad58c3874a3f8d8ce01
Vitor Tomic <vitortomic@gmail.com>
2017-10-12 18:34:23 +0200

    Update fetch.js

--- javascripts/fetch.js
+++ javascripts/fetch.js
@@ -1,3 +1,11 @@
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response=>response.json())
   .then((json)=>console.log(json))
+
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response=>response.json())
+.then((json)=>{
+    var result = json.filter((element)=>element.id<10).map((element)=> element.title).join(", ");
+    console.log(result)
+})
