05bc5a6492e0cdc688e5cc982384685c179a40db
Vitor Tomic <vitortomic@gmail.com>
2018-07-19 12:29:16 +0200

    Update delay.js

--- javascripts/delay.js
+++ javascripts/delay.js
@@ -2,7 +2,7 @@ const delayFor = (delay) => new Promise(resolve =>
 	setTimeout(()=>resolve(),delay)
 )
 
-#delayFor(4000).then(completed=>console.log("completed"))
+//delayFor(4000).then(completed=>console.log("completed"))
 
 const delayedLoop = async (iterations, delay)=> {
 	for (let i = 0; i < iterations; i++) {
